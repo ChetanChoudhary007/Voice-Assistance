6de15d49d0b6431b50c0b56837b215e59f91f595
ChetanChoudhary007 <cc393653@gmail.com>
2023-08-19 12:32:02 +0530

    First commit

---
+++ supporter.py
@@ -0,0 +1,15 @@
+
+import random 
+
+import selenium
+
+
+def contact_list(name):
+#list of contacts..
+    contact={
+            #name : #phone number
+            }
+    return contact[f"{name}"]
+    
+if __name__ == "__main__":
+    main()
